8e42970e96340023658c79e991365b5a11740634
tudm-0884 <dao.minh.tu@sun-asterisk.com>
2019-10-19 18:41:11 +0700

    update login blade

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -48,4 +48,18 @@ class LoginController extends Controller
     {
         return view('admin.login');
     }
+
+    /**
+     * Get the post register / login redirect path.
+     *
+     * @return string
+     */
+    public function redirectPath()
+    {
+        if (method_exists($this, 'redirectTo')) {
+            return $this->redirectTo();
+        }
+
+        return property_exists($this, 'redirectTo') ? $this->redirectTo : '/';
+    }
 }
